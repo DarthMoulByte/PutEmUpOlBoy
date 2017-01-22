431ee5fdcb27db652dd23b5a45fd7d5aa3a31a10
Bj�rnar <bjornar24@gmail.com>
2017-01-22 05:00:31 +0100

    audio script and files

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+	public static Audio Instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = FindObjectOfType<Audio>();
+			}
+
+			return _instance;
+		}
+	}
+
+	private static Audio _instance;
+
+	public AudioSource three;
+	public AudioSource two;
+	public AudioSource one;
+	public AudioSource go;
+	public AudioSource tickTock;
+	public AudioSource skeletonSmack;
+	public AudioSource skeletonDeath;
+	public AudioSource skeletonCheer;
+	public AudioSource waveSucessful;
+	public AudioSource boooo;
+
+	public AudioSource ambience;
+	public AudioSource skeletonSad;
+	public AudioSource mainMenu;
+	public AudioSource ingameMusic;
+	public AudioSource winGame;
+	public AudioSource losegame;
+
+	public static void PlayAudioSource(AudioSource audioSource, float fadeInTime = 0f)
+	{
+		if (audioSource != null && !audioSource.isPlaying)
+		{
+			if (fadeInTime == 0)
+			{
+				audioSource.Play();
+			}
+			else
+			{
+				_instance.StartCoroutine( _instance.FadeInAudioSource( audioSource , fadeInTime ) );
+			}
+		}
+	}
+
+	public static void StopAudioSource(AudioSource audioSource, float fadeOutTime = 0f)
+	{
+		if (audioSource != null)
+		{
+			if (fadeOutTime == 0)
+			{
+				audioSource.Stop();
+			}
+			else
+			{
+				_instance.StartCoroutine(_instance.FadeOutAudioSource(audioSource, fadeOutTime));
+			}
+		}
+	}
+
+	private IEnumerator FadeOutAudioSource(AudioSource audioSource, float time)
+	{
+		var originalVolume = audioSource.volume;
+
+		time = Mathf.Max(0.01f, time);
+		originalVolume = Mathf.Max(0.01f, originalVolume );
+
+		var ratio = originalVolume/time;
+
+		while (audioSource.volume >= 0f)
+		{
+			audioSource.volume -= ratio * Time.deltaTime;
+			if (audioSource.volume <= 0f) break;
+			yield return null;
+		}
+
+		audioSource.Stop();
+		audioSource.volume = originalVolume;
+
+		yield return null;
+	}
+
+	private IEnumerator FadeInAudioSource(AudioSource audioSource, float time)
+	{
+		var originalVolume = audioSource.volume;
+
+		audioSource.volume = 0f;
+		audioSource.Play();
+
+		time = Mathf.Max(0.01f, time);
+		originalVolume = Mathf.Max(0.01f, originalVolume );
+
+		var ratio = originalVolume/time;
+
+		while (audioSource.volume >= 0f)
+		{
+			audioSource.volume += ratio * Time.deltaTime;
+			if (audioSource.volume >= originalVolume) break;
+			yield return null;
+		}
+
+		audioSource.volume = originalVolume;
+
+		yield return null;
+	}
+
+
+	private void PlayRandomClipFromList(List<AudioSource> list)
+	{
+		list[ Random.Range( 0 , list.Count ) ].Play();
+	}
+
+
+}
